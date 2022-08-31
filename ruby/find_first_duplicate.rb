383e5bd2dd2bcf78d7b5e25a96b0cb10eddbc600
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-08-31 17:45:25 -0500

    new version

--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,11 +1,20 @@
+require 'pry'
+
 def find_first_duplicate(arr)
-  uniques = Set.new
-  arr.each do |val|
-    return val if uniques.include?(val)
-    uniques.add(val)
+  seen = {}
+  arr.each do |x|
+    if !seen[x]
+      seen[x] = 1
+    else
+      return x
+    end
+    p seen
   end
-  -1
+  return -1
 end
+
+arr1 = [2, 1, 3, 3, 2]
+arr2 = [1, 2, 3, 4]
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 3"
@@ -19,5 +28,21 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+/
+1. Understand the problem
+    a. What datatypes are we given? => Array of integers
+    b. What datatype is the return supposed to be? => Integer
+    c. Bonus - identify potential stretch cases 
+2. Pseudocode
+    a. Run with your first thought
+        Loop through array
+          log each element
+          check if we've seen this element before
+          if yes:
+            break the loop
+            return this element
+          if no:
+            continue loop
+          if loop ends with no match:
+            return -1
+/
